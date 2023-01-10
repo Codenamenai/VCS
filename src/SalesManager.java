1ea3f6171999911dd9b7ec7f9802a95678dd2eed
Anton Nazarov <antonionazarini@yandex.ru>
2023-01-10 17:42:23 +0300

    обрезансреднезнач изменен гитигнор

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,5 +14,21 @@ public class SalesManager {
             }
         }
         return max;
+    }public int min() {
+        int min = 10000;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }public int sum() {
+        int sum=0;
+        for (int i = 0; i < sales.length ; i++) {
+            sum+=sales[i];
+            
+        }
+        return sum;
     }
+
 }
